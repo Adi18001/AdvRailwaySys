a6c984467c436451f9b3404b61d1d93e665b45f2
Aditya Andhale <adityaandhale63@gmail.com>
2024-09-23 01:00:40 +0530

    new change

--- hellonew.java
+++ hellonew.java
@@ -1,6 +1,8 @@
 public class hellonew {
     public static void main(String [] args){
         System.out.println("hello group members !!!!!!!");
+        int a=10;
+        System.out.println(a);
     }
     
 }
